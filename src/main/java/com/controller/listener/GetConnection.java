012cb9ec6418e754f52c56b23b82aac48cfecae1
hongxiaobin <2812181610@qq.com>
2022-06-09 17:52:33 +0800

    2812181610@qq.com

---
+++ src/main/java/com/controller/listener/GetConnection.java
@@ -0,0 +1,29 @@
+package com.controller.listener;
+
+import com.common.JdbcConnection;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import java.sql.Connection;
+
+/**
+ * @Author hongxiaobin
+ * @Time 2022/6/9-17:07
+ */
+public class GetConnection implements ServletContextListener {
+    public static Connection connection;
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        try {
+            connection = JdbcConnection.getConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        connection = null;
+    }
+}
--- src/main/java/com/dao/AdminDaoImpl.java
+++ src/main/java/com/dao/AdminDaoImpl.java
@@ -1,6 +1,7 @@
 package com.dao;
 
 import com.common.JdbcConnection;
+import com.controller.listener.GetConnection;
 import com.dao.implement.AdminDap;
 import com.entity.AdminModel;
 
@@ -12,15 +13,7 @@ import java.util.List;
  * @Time 2022/6/5-14:07
  */
 public class AdminDaoImpl extends BaseDao<AdminModel> implements AdminDap {
-    Connection connection;
-
-    {
-        try {
-            connection = JdbcConnection.getConnection();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+    Connection connection = GetConnection.connection;
     @Override
     public List<AdminModel> getAll(AdminModel adminModel) {
         String sql = "select * from admin where aid = ? and apassword = ?";
